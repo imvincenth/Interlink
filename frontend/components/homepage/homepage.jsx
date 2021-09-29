f1d991d4a081b0085606206034fce855a3d49a7c
Vincent Hsu <vincesuuu1@gmail.com>
2021-09-29 07:23:27 -0700

    Basic styling done to the homepage

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../../../app/assets/images/namelogo.svg";
+import Hero from "../../../app/assets/images/homepage.svg";
 
-const Homepage = ({ currentUser, logout }) => {
-  const sessionLinks = () => (
-    <Link to="/">
-      <img src={Logo} alt="login logo" />
-    </Link>
-  );
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div className="homepage">
+        <nav className="homepage-nav">
+          <a href="/">
+            <img src={Logo} className="login-logo" alt="login logo" />
+          </a>
 
-  const personalGreeting = () => (
-    <hgroup className="header-group">
-      <h2 className="header-name">Hi, {currentUser.first_name}!</h2>
-      <button className="header-button" onClick={logout}>Log Out</button>
-    </hgroup>
-  );
+          <div>
+            <Link className="joinnow-nav" to="/signup">Join Now</Link>
+            <Link className="signup-nav" to="/login">Sign In</Link>
+          </div>
+        </nav>
 
-  return currentUser ? personalGreeting() : sessionLinks();
-};
+        <section class="homepage-main">
+          <p>One more step and it'll be the farthest I've ever been from home</p>
+          <img src={Hero} className="homepage-picture" alt="hero picture" />
+        </section>
+      </div>
+    )
+  }
+}
 
 export default Homepage;
