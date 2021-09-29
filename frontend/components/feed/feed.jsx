762b811074fbeaae689471bbbb338bcaff9e8460
Vincent Hsu <vincesuuu1@gmail.com>
2021-09-29 16:09:14 -0700

    Fixed name logo showing up in the feed page

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import Logo from "../../../app/assets/images/namelogo.svg";
 
 class Feed extends React.Component {
   render() {
     const { currentUser, logout } = this.props;
     return (
       <div className="header-group">
-        <img src={Logo} className="login-logo" alt="login logo" />
+        <img src={namelogoURL} className="login-logo" alt="login logo" />
         <h2 className="header-name">Hi, {currentUser.first_name} {currentUser.last_name}!</h2>
         <button className="header-button" onClick={logout}>Log Out</button>
     </div>
