05a53d52a59515032a353a2b10a9ea33d37c8fae
Vincent Hsu <vincesuuu1@gmail.com>
2021-09-29 23:36:04 -0700

    Started creating the header component

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -5,11 +5,16 @@ class Feed extends React.Component {
   render() {
     const { currentUser, logout } = this.props;
     return (
-      <div className="header-group">
+      <div className="feed-page">
+        <div>
+          <header>
+            Content
+          </header>
+        </div>
         <img src={logoURL} className="login-logo" alt="login logo" />
         <h2 className="header-name">Hi, {currentUser.first_name} {currentUser.last_name}!</h2>
         <button className="header-button" onClick={logout}>Log Out</button>
-    </div>
+      </div>
     )
   }
 }
