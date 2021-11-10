e2940fecdb133799dffb8ef2410a8c88a3c9dbc1
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-09 17:48:40 -0800

    Adding the education form switch to signup page four

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const positions = "Full-time,Part-time,Self-employed,Freelance,Contract,Internship,Apprenticeship,Seasonal";
+const years = "2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000,1999,1998,1997,1996,1995,1994,1993,1992,1991,1990,1989,1988,1987,1986,1985,1984,1983,1982,1981,1980,1979,1978,1977,1976,1975,1974,1973,1972,1971,1970,1969,1968,1967,1966,1965,1964,1963,1962,1961,1960,1959,1958,1957,1956,1955,1954,1953,1952,1951,1950,1949,1948,1947,1946,1945,1944,1943,1942,1941,1940,1939,1938,1937,1936,1935,1934,1933,1932,1931,1930,1929,1928,1927,1926,1925,1924,1923,1922,1921,1920,1919,1918,1917,1916,1915,1914,1913,1912,1911,1910,1909,1908,1907,1906,1905,1904,1903,1902,1901,1900";
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class SignupForm extends React.Component {
       country_region: "",
       city_district: "",
       visiblePage: 1,
+      // Student Check
       student: false,
 
       currentUser: this.props.currentUser,
@@ -41,9 +43,7 @@ class SignupForm extends React.Component {
       grade: "",
       extracurriculars: "",
 
-      startMon: "",
       startYr: "",
-      endMon: "",
       endYr: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -250,6 +250,30 @@ class SignupForm extends React.Component {
       </div>
     )
   }
+  
+  setStartTime() {
+    this.setState({ 
+      start_date: `${this.state.startYr}`
+    });
+  }
+
+  setEndTime() {
+    if (!this.state.student) {
+      this.setState({
+        end_date: "Present"
+      });
+    } else {
+      this.setState({
+        end_date: `${this.state.endYr}`
+      });
+    }
+  }
+
+  setTimes() {
+    if (this.state.start_date === "-" || this.state.end_date === "-") throw "Start and end dates are required";
+    this.setStartTime();
+    this.setEndTime();
+  }
 
   companyErrors() {
     let companyErrors = [];
@@ -283,6 +307,22 @@ class SignupForm extends React.Component {
     }
   }
 
+  schoolErrors() {
+    let schoolErrors = [];
+    if (this.props.errors.length !== 0) {
+      this.props.errors.forEach(error => {
+        if (error.includes("School")) {
+          schoolErrors.push(error);
+        }
+      })
+      return (
+        <span className="error">
+          {schoolErrors}
+        </span>
+      );
+    }
+  }
+
   pageFour() {
     if (!this.state.student) {
       return (
@@ -291,26 +331,26 @@ class SignupForm extends React.Component {
             <h1 className="signup-greeting-two">Your profile helps you discover new people and opportunities</h1>
           </header>
           <div className="signup-spacing"></div>
-  
-          <div className="signup-form-box-four">
 
-            <div className="signup-form-spacing">
-              <label className="signup-label">Most recent job title? <span className="blue-star">*</span>
-                <input type="text"
-                  value={this.state.headline}
-                  onChange={this.update('headline')}
-                  className="signup-input"
-                />
-              </label>
-              {this.checkErrors === 2 ? "" : this.renderHeadlineError()}
+            <div className="signup-form-box-four">
+              <div className="signup-form-spacing">
+                <label className="signup-label">Most recent job title? <span className="blue-star">*</span>
+                  <input type="text"
+                    value={this.state.headline}
+                    onChange={this.update('headline')}
+                    className="signup-input"
+                  />
+                </label>
+                {this.checkErrors === 2 ? "" : this.renderHeadlineError()}
+              </div>
+
+              {this.state.headline.length > 0 ? this.typeCompanyForms() : null}
+              {this.state.company.length > 0 && this.state.headline.length > 0 ? this.industryForms() : null}
+
+              <button className="student-switch" onClick={this.yesStudent}>I’m a student</button>
+              {this.state.headline.length > 0 && this.state.industry.length > 0 ? this.canContinue() : this.cantContinue()}
             </div>
-
-            {this.state.headline.length > 0 ? this.typeCompanyForms() : null}
-            {this.state.company.length > 0 && this.state.headline.length > 0 ? this.industryForms() : null}
-
-            <button className="student-switch" onClick={this.yesStudent}>I’m a student</button>
-            {this.state.headline.length > 0 && this.state.industry.length > 0 ? this.canContinue() : this.cantContinue()}
-          </div>
+  
         </div>
       )
     } else {
@@ -320,6 +360,66 @@ class SignupForm extends React.Component {
             <h1 className="signup-greeting-two">Your profile helps you discover new people and opportunities</h1>
           </header>
 
+            <div className="signup-form-box-four">
+
+              <div>
+                <label className="signup-label">School or College/University <span className="blue-star">*</span>
+                  <input 
+                    type="text" 
+                    value={this.state.school} 
+                    onChange={this.update("school")} 
+                    className="signup-input"
+                  />
+                </label>
+                {this.schoolErrors()}
+              </div>
+
+              <div>
+                <label className="signup-label">Degree <span className="blue-star">*</span>
+                  <input 
+                    value={this.state.degree} 
+                    onChange={this.update("degree")} 
+                    className="signup-input"
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label className="signup-label">Specialization <span className="blue-star">*</span>
+                  <input 
+                    type="text" 
+                    value={this.state.subject} 
+                    onChange={this.update("subject")} 
+                    className="signup-input"
+                  />
+                </label>
+              </div>
+
+              <div className="signup-student-dates">
+                <div>
+                  <label className="signup-label">Start date <span className="blue-star">*</span>
+                    <div>
+                      <select onChange={this.update("startYr")}  className="signup-dropdown">
+                        <option> - </option>
+                        {this.createOptions(years)}
+                      </select>
+                    </div>
+                  </label>
+                </div>
+
+                <div>
+                  <label className="signup-label">End date (or expected) <span className="blue-star">*</span>
+                    <div>
+                      <select onChange={this.update("endYr")}  className="signup-dropdown">
+                        <option> - </option>
+                        {this.createOptions(years)}
+                      </select>
+                    </div>
+                  </label>
+                </div>
+              </div>
+
+            </div>
 
           <button className="student-switch" onClick={this.notStudent}>I’m not a student</button>
         </div>
