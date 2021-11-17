0c08ee72ff54e5ed87a7eeab8382cae49faac499
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-17 13:39:03 -0800

    Created the post api util and post actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,8 @@
+import * as PostApiUtil from '../util/post_api_util';
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+export const RECEIVE_POST_ERRORS = 'RECEIVE_POST_ERRORS';
+export const REMOVE_ERRORS = 'REMOVE_ERRORS';
+
---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,29 @@
+export const createPost = post => (
+  $.ajax({
+    url: `/api/posts`,
+    method: "POST",
+    data: { post }
+  })
+);
+
+export const fetchPosts = () => (
+  $.ajax({
+    url: `/api/posts`,
+    method: "GET"
+  })
+);
+
+export const updatePost = post => (
+  $.ajax({
+    url: `/api/posts/${post.id}`,
+    method: "PATCH",
+    data: { post }
+  })
+);
+
+export const deletePost = postId => (
+  $.ajax({
+    url: `/api/posts/${postId}`,
+    method: "DELETE"
+  })
+);
