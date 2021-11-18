8e8603aef6e8fa3c28675ca1d17577c8719c5173
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-17 16:25:21 -0800

    Built out posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,35 @@
 class Api::PostsController < ApplicationController
 
-  
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render :show
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @posts = Post.all
+    render :index
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if @post && @post.update(post_params)
+      render :show
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post
+      @post.destroy
+    end
+  end
 
   private
   def post_params
