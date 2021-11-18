65e92c8195274bfad8d4c8ed34750a0dcf4f2ad6
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-17 16:12:56 -0800

    Created model validations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,9 @@
 #  index_posts_on_user_id  (user_id)
 #
 class Post < ApplicationRecord
+  validates :user_id, :body, presence: true
+
+  belongs_to :user,
+    foreign_key: :user_id,
+    class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,11 @@ class User < ApplicationRecord
     class_name: :Education,
     dependent: :destroy
 
+  has_many :posts,
+    foreign_key: :user_id,
+    class_name: :Post,
+    dependent: :destroy
+
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     return nil unless user
