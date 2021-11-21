9b14839c01211148f7d85843cc4fc1a3324beb97
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-20 23:43:00 -0800

    Turning the create post into a modal

--- frontend/components/feed/post/create_post_container.js
+++ frontend/components/feed/post/create_post_container.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createPost } from '../../../actions/post_actions';
+import { closeModal } from '../../../actions/modal_actions';
 import Post from './create_post';
 
 const mSTP = ({ session, entities: { users } }) => ({
@@ -9,7 +10,8 @@ const mSTP = ({ session, entities: { users } }) => ({
 
 const mDTP = dispatch => ({
   fetchUser: userId => dispatch(fetchUser(userId)),
-  createPost: post => dispatch(createPost(post))
+  createPost: post => dispatch(createPost(post)),
+  closeModal: () => dispatch(closeModal()),
 });
 
 export default connect(mSTP, mDTP)(Post);
