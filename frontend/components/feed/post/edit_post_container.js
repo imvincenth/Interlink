19ccb497de4c1fc5c7aeb9eb7f1976cedd9c9395
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-21 21:23:10 -0800

    Building out edit post form

--- frontend/components/feed/post/edit_post_container.js
+++ frontend/components/feed/post/edit_post_container.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { updatePost, deletePost } from '../../../actions/post_actions';
+
 import PostEdit from './edit_post';
 
 const mSTP = ({ session, entities: { users } }) => ({
-    currentUser: users[session.id]
+  currentUser: users[session.id]
 });
 
 const mDTP = dispatch => ({
-  fetchUser: userId => dispatch(fetchUser(userId))
+  
 });
 
 export default connect(mSTP, mDTP)(PostEdit);
