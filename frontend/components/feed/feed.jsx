b62af0cd26f31eec7696d272056fb6f51865a43f
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-24 02:45:19 -0800

    Revert "Trying to correctly render deleted posts"
    This reverts commit 332208ddbf89a37c966b73baca488c3b9e44e90d.

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -11,7 +11,8 @@ class Feed extends React.Component {
     this.state = {
       posts: []
     }
-  
+
+    
   }
 
   componentDidMount() {
@@ -19,13 +20,11 @@ class Feed extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.posts !== this.props.posts || prevState.posts !== this.state.posts) {
-      this.fillPosts();
-    };
+    
   }
 
   fillPosts() {
-    this.props.posts.forEach(post => post.parent_id === -1 && !this.state.posts.includes(post) ? this.setState({ posts: [...this.state.posts, post] }) : null);
+    this.props.posts.forEach(post => post.parent_id === -1 ? this.setState({ posts: [...this.state.posts, post] }) : null);
   }
 
   render() {
