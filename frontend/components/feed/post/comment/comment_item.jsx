e820acc2f09b67c837c1992f3b16f516b4137805
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-24 06:43:44 -0800

    Comment edit now functioning, memory leak issue

--- frontend/components/feed/post/comment/comment_item.jsx
+++ frontend/components/feed/post/comment/comment_item.jsx
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
 
 export default class Comment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...this.props.comment,
+
+      editField: false
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchComments(this.state.post_id);
+  }
+
+  componentWillUnmount() {
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.updateComment({...this.state})
+      .then(() => this.toggleEdit());
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  toggleEdit() {
+    this.setState({ editField: !this.state.editField });
+  }
+
+  editForm() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.body} onChange={this.update("body")} />
+          <input type="submit" value="Edit reply" />
+        </form>
+      </div>
+    )
+  }
+
   render() {
     const { comment } = this.props;
     return (
       <div className="comment-box">
-        {comment.body}
-        <button>
+        reply: {comment.body}
+        <button onClick={this.toggleEdit}>
           <img src={window.vectorURL} alt="pen" />
         </button>
+        {this.state.editField ? this.editForm() : null}
         <button onClick={() => this.props.deleteComment(comment.id)}>
           Delete
         </button>
--- frontend/components/feed/post/comment/comment_item_container.js
+++ frontend/components/feed/post/comment/comment_item_container.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchComments, deleteComment } from '../../../../actions/comment_actions';
-import { openModal } from '../../../../actions/modal_actions';
+import { fetchComments, updateComment, deleteComment } from '../../../../actions/comment_actions';
 import Comment from './comment_item';
 
 const mSTP = state => ({
@@ -10,7 +9,7 @@ const mSTP = state => ({
 
 const mDTP = dispatch => ({
   fetchComments: () => dispatch(fetchComments()),
-  openEditCommentModal: comment => dispatch(openModal("editComment", comment)),
+  updateComment: comment => dispatch(updateComment(comment)),
   deleteComment: comment => dispatch(deleteComment(comment))
 });
 
