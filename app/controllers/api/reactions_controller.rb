c58dd770114ce6301b7782858ef313e0b3675c98
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-29 16:23:54 -0800

    Building out reaction views

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -1,11 +1,11 @@
 class Api::ReactionsController < ApplicationController
 
   def index
-    if params[:commentId]
-      comment = Comment.find_by(id: params[:commentId])
+    if params[:comment_id]
+      comment = Comment.find_by(id: params[:comment_id])
       @reactions = Reaction.where(reactable_type: :Comment, reactable_id: comment.id)
-    elsif params[:postId]
-      post = Post.find_by(id: params[:postId])
+    elsif params[:post_id]
+      post = Post.find_by(id: params[:post_id])
       @reactions = Reaction.where(reactable_type: :Post, reactable_id: post.id)
     else
       render json: { error: "No reactions for this post/comment" }, status: 404
@@ -19,6 +19,39 @@ class Api::ReactionsController < ApplicationController
       render :show
     else
       render json: @reaction.errors.full_messages, status: 404
+    end
+  end
+
+  def create
+    if params[:comment_id]
+      comment = Comment.find_by(id: params[:comment_id])
+      @reaction = Reaction.new(reaction_params)
+    elsif params[:post_id]
+      post = Post.find_by(id: params[:post_id])
+      @reaction = Reaction.new(reaction_params)
+    end
+    if @reaction && @reaction.save
+      render :show
+    else
+      render json: @reaction.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @reaction = Reaction.find_by(id: params[:id])
+    if @reaction && @reaction.update(reaction_params)
+      render :show
+    else
+      render @reaction.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @reaction = Reaction.find_by(id: params[:id])
+    if @reaction && @reaction.destroy
+      render :show
+    else
+      render @reaction.errors.full_messages, status: 422
     end
   end
 
