ed149e92e71cfb660b10512cc401320e9853aab4
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-29 16:01:57 -0800

    Building out reactions controller

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -1,7 +1,25 @@
 class Api::ReactionsController < ApplicationController
 
   def index
-    if params[:]
+    if params[:commentId]
+      comment = Comment.find_by(id: params[:commentId])
+      @reactions = Reaction.where(reactable_type: :Comment, reactable_id: comment.id)
+    elsif params[:postId]
+      post = Post.find_by(id: params[:postId])
+      @reactions = Reaction.where(reactable_type: :Post, reactable_id: post.id)
+    else
+      render json: { error: "No reactions for this post/comment" }, status: 404
+    end
+    render :index
+  end
+
+  def show
+    @reaction = Reaction.find_by(id: params[:id])
+    if @reaction
+      render :show
+    else
+      render json: @reaction.errors.full_messages, status: 404
+    end
   end
 
   private
