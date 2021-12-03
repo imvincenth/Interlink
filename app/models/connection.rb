3780b3df550764465b593f9039c4efb78cb44107
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-03 10:17:40 -0800

    Trying to debug the connection model for Heroku deployment

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -19,7 +19,7 @@ class Connection < ApplicationRecord
 
   belongs_to :connector,
     foreign_key: :connector_id,
-    class_name: :User,
+    class_name: :User
 
   belongs_to :connectee,
     foreign_key: :connectee_id,
