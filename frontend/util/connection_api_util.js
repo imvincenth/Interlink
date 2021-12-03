c967cea3df48d06abddc79db73d4ecbd6f1b4961
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-03 01:35:19 -0800

    Commented back in the connection model and api util for Heroku test

--- frontend/util/connection_api_util.js
+++ frontend/util/connection_api_util.js
@@ -1,29 +1,29 @@
-// export const createConnection = connection => (
-//   $.ajax({
-//     url: `/api/connections`,
-//     method: "POST",
-//     data: { connection }
-//   })
-// );
+export const createConnection = connection => (
+  $.ajax({
+    url: `/api/connections`,
+    method: "POST",
+    data: { connection }
+  })
+);
 
-// export const fetchConnections = userId => (
-//   $.ajax({
-//     url: `/api/posts/${userId}/connections`,
-//     method: "GET"
-//   })
-// );
+export const fetchConnections = userId => (
+  $.ajax({
+    url: `/api/posts/${userId}/connections`,
+    method: "GET"
+  })
+);
 
-// export const updateConnection = connection => (
-//   $.ajax({
-//     url: `/api/connections/${connection.id}`,
-//     method: "PATCH",
-//     data: { connection }
-//   })
-// );
+export const updateConnection = connection => (
+  $.ajax({
+    url: `/api/connections/${connection.id}`,
+    method: "PATCH",
+    data: { connection }
+  })
+);
 
-// export const deleteConnection = connectionId => (
-//   $.ajax({
-//     url: `/api/connections/${connectionId}`,
-//     method: "DELETE"
-//   })
-// );
+export const deleteConnection = connectionId => (
+  $.ajax({
+    url: `/api/connections/${connectionId}`,
+    method: "DELETE"
+  })
+);
