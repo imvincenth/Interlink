6ecd23d70979dbfc3c777274a302d3f231394539
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-03 01:29:15 -0800

    Commenting out connections backend and frontend for Heroku deployment test

--- frontend/reducers/connections_errors_reducer.js
+++ frontend/reducers/connections_errors_reducer.js
@@ -1,25 +1,25 @@
-import { 
-  RECEIVE_CONNECTION_ERRORS,
-  REMOVE_CONNECTION,
-  REMOVE_ERRORS
-} from "../actions/connection_actions"
+// import { 
+//   RECEIVE_CONNECTION_ERRORS,
+//   REMOVE_CONNECTION,
+//   REMOVE_ERRORS
+// } from "../actions/connection_actions"
 
-const connectionErrorsReducer = (oldState = [], action) => {
-  Object.freeze(oldState);
+// const connectionErrorsReducer = (oldState = [], action) => {
+//   Object.freeze(oldState);
 
-  switch(action.type) {
-    case RECEIVE_CONNECTION_ERRORS:
-      return action.errors;
+//   switch(action.type) {
+//     case RECEIVE_CONNECTION_ERRORS:
+//       return action.errors;
 
-    case REMOVE_CONNECTION:
-      return [];
+//     case REMOVE_CONNECTION:
+//       return [];
 
-    case REMOVE_ERRORS:
-      return [];
+//     case REMOVE_ERRORS:
+//       return [];
 
-    default:
-      return oldState;
-  }
-}
+//     default:
+//       return oldState;
+//   }
+// }
 
-export default connectionErrorsReducer;
+// export default connectionErrorsReducer;
