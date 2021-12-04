c1bf5c23c3f48f13a63f51546530abac9a789e27
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-04 04:37:10 -0800

    Deciding on where to filter connections in controller

--- app/controllers/api/connections_controller.rb
+++ app/controllers/api/connections_controller.rb
@@ -1,39 +1,39 @@
-# class Api::ConnectionsController < ApplicationController
+class Api::ConnectionsController < ApplicationController
 
-#   def create
-#     @connection = Connection.new(connection_params)
+  def create
+    @connection = Connection.new(connection_params)
 
-#     if @connection.save
-#       render :show
-#     else
-#       render json: @connection.errors.full_messages, status: 422
-#     end
-#   end
+    if @connection.save
+      render :show
+    else
+      render json: @connection.errors.full_messages, status: 422
+    end
+  end
 
-#   def index
-#     # @connections = Connection.find_by(connectee_id: params[:connectee_id], connector_id: params[:connector_id])
-#     @connections = Connection.all
-#     render :index
-#   end
+  def index
+    @connections = Connection.all
+    
+    render :index
+  end
 
-#   def update
-#     @connection = Connection.find_by(id: params[:id])
-#     if @connection && @connection.update(connection_params)
-#       render :show
-#     else
-#       render json: @connection.errors.full_messages, status: 422
-#     end
-#   end
+  def update
+    @connection = Connection.find_by(id: params[:id])
+    if @connection && @connection.update(connection_params)
+      render :show
+    else
+      render json: @connection.errors.full_messages, status: 422
+    end
+  end
 
-#   def destroy
-#     @connection = Connection.find_by(id: params[:id])
-#     if @connection
-#       @connection.destroy
-#     end
-#   end
+  def destroy
+    @connection = Connection.find_by(id: params[:id])
+    if @connection
+      @connection.destroy
+    end
+  end
 
-#   private
-#   def connection_params
-#     params.require(:connection).permit(:status, :connector_id, :connectee_id)
-#   end
-# end
+  private
+  def connection_params
+    params.require(:connection).permit(:status, :connector_id, :connectee_id)
+  end
+end
