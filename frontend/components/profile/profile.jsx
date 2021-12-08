1feefee3bfc96faffb487f5abb0cd7c9fd6c1145
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-08 08:38:49 -0800

    Revert "Refactoring code to be more efficient"
    This reverts commit 5fb7b6ade01204283700a76d646260d311910474.

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -17,7 +17,6 @@ class Profile extends React.Component {
     }
 
     this.connection;
-    this.connectionCount = this.props.connections.length;
     this.handleConnectSubmit = this.handleConnectSubmit.bind(this);
     this.handleEditConnect = this.handleEditConnect.bind(this);
   }
@@ -143,7 +142,6 @@ class Profile extends React.Component {
       if (this.props.connections[i].connector_id === this.props.currentUser.id || this.props.connections[i].connectee_id === this.props.currentUser.id) {
         if (this.props.connections[i].pending) {
           this.connection = this.props.connections[i];
-          this.connectionCount = this.props.connections.length;
         }
       }
     };
@@ -165,7 +163,9 @@ class Profile extends React.Component {
     } else {
       existingConnection = false;
     }
-    console.log(this.connectionCount);
+
+    console.log(this.props.connections.length);
+
     return (
       <div className="profile-background">
         <div className="profile-container">
