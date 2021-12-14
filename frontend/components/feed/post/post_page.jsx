9efc8a72afd55a1d057700b71f285d6f395b3bd4
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-13 23:07:09 -0800

    Building fronend AWS image upload

--- frontend/components/feed/post/post_page.jsx
+++ frontend/components/feed/post/post_page.jsx
@@ -32,6 +32,19 @@ export default class Post extends Component {
       }
     }
   }
+
+  handleImageUpload() {
+    const reader = new FileReader();
+    const file = e.currentTarget.files[0];
+    reader.onloadend = () =>
+      this.setState({ imageUrl: reader.result, imageFile: file });
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      this.setState({ imageUrl: "", imageFile: null });
+    }
+  }
   
   componentDidMount() {
     // this.props.fetchPost(this.props.postId)
