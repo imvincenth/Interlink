3b5710990067d8dc342b119182bdab681e1e2575
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-18 05:17:22 -0800

    Fixed opacity inconsistencies in the navbar

--- frontend/components/feed/navbar/navbar.jsx
+++ frontend/components/feed/navbar/navbar.jsx
@@ -34,15 +34,15 @@ class Navbar extends React.Component {
 
         <div className="navbar-list">
           <div className="navbar-item">
-            <Link className={this.props.page === "feed" ? "navbar-link transparent active" : "navbar-link transparent"} to="/feed">
-              <img className={this.props.page === "feed" ? "navbar-icon transparent active" : "navbar-icon transparent"} src={window.feedURL} alt="feed url" />
+            <Link className={this.props.page === "feed" ? "navbar-link active" : "navbar-link transparent"} to="/feed">
+              <img className={this.props.page === "feed" ? "navbar-icon active" : "navbar-icon"} src={window.feedURL} alt="feed url" />
               <h5>Home</h5>
             </Link>
           </div>
 
           <div className="navbar-item">
-            <Link className={this.props.page === "network" ? "navbar-link transparent active" : "navbar-link transparent"} to="/network">
-              <img className={this.props.page === "network" ? "navbar-icon transparent active" : "navbar-icon transparent"} src={window.networkURL} alt="feed url" />
+            <Link className={this.props.page === "network" ? "navbar-link active" : "navbar-link transparent"} to="/network">
+              <img className={this.props.page === "network" ? "navbar-icon active" : "navbar-icon"} src={window.networkURL} alt="feed url" />
               <h5>Network</h5>
             </Link>
           </div>
@@ -64,7 +64,7 @@ class Navbar extends React.Component {
           <div className="navbar-item">
             <Link className={this.props.page === "profile" ? "navbar-link active" : "navbar-link"} to={`/users/${this.props.currentUser.id}`}>
               {this.props.currentUser.profilePhotoUrl ? <img className="navbar-icon-profile" src={this.props.currentUser.profilePhotoUrl} alt="feed url" /> : <img className="navbar-icon-profile" src="https://static-exp1.licdn.com/sc/h/1c5u578iilxfi4m4dvc4q810q" alt="default profile picture" />}
-              <h5 className={this.props.page === "profile" ? "navbar-me active" : "navbar-link"}>Me<img className="navbar-arrow" src={window.downarrowURL} alt="down arrow" /></h5>
+              <h5 className={this.props.page === "profile" ? "navbar-me active" : "navbar-link transparent"}>Me<img className="navbar-arrow" src={window.downarrowURL} alt="down arrow" /></h5>
             </Link>
           </div>
         </div>
