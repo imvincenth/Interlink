0d3ef448fc9ec21e27afe5be3bea73334ccb21ce
Vincent Hsu <vincesuuu1@gmail.com>
2021-12-27 19:08:39 -0800

    Completed sidebar header, banner, and avatar

--- frontend/components/feed/sidebar/sidebar.jsx
+++ frontend/components/feed/sidebar/sidebar.jsx
@@ -25,19 +25,28 @@ class Sidebar extends React.Component {
             {/* Banner */}
             <div className='sidebar-banner'>
               {this.props.currentUser.bannerUrl ? 
-              <img className='sidebar-banner-img' src={this.props.currentUser.bannerUrl} alt='sidebar banner' /> : 
-              <img className='sidebar-banner-img' src='https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq' alt='sidebar banner' />
+                <img className='sidebar-banner-img' src={this.props.currentUser.bannerUrl} alt='sidebar banner' /> : 
+                <img className='sidebar-banner-img' src='https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq' alt='sidebar banner' />
               }
             </div>
 
             {/* Avatar */}
-            <div className='sidebar-avatar'>
-              {this.props.currentUser.profilePictureUrl ? <img src={this.props.currentUser.profilePictureUrl} alt='user profile picture' /> : <img src='https://static-exp1.licdn.com/sc/h/3h0vrtch1zepjr4p54aja8i9x' alt='default profile picture' />}
+            <div>
+              {this.props.currentUser.profilePictureUrl ? <img className='sidebar-avatar' src={this.props.currentUser.profilePictureUrl} alt='user profile picture' /> : <img src='https://static-exp1.licdn.com/sc/h/3h0vrtch1zepjr4p54aja8i9x' alt='default profile picture' />}
             </div>
 
+            {/* Current User Info */}
+            {this.props.currentUser.profilePictureUrl ? 
+              <div>
+                <Link className='sidebar-name' to={`/users/${this.props.currentUser.id}`}>{this.props.currentUser.first_name} {this.props.currentUser.last_name}</Link>
+              </div> : 
+              <div>
+                <Link className='sidebar-name' to={`/users/${this.props.currentUser.id}`}>Welcome, {this.props.currentUser.first_name}!</Link>
+              </div>
+            }
+            
+            <p className='sidebar-headline'>{this.props.currentUser.headline}</p>
 
-            <h2 className='sidebar-name'><Link className='sidebar-name-link' to={`/users/${this.props.currentUser.id}`}>{this.props.currentUser.first_name} {this.props.currentUser.last_name}</Link></h2>
-            <h4 className='sidebar-email'>{this.props.currentUser.headline}</h4>
           </div>
 
           <div className='sidebar-info-wrap'>
