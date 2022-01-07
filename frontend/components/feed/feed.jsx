c8473bc1c70cb4d4a2b9e41fbb9bf15e0013781f
Vincent Hsu <vincesuuu1@gmail.com>
2022-01-07 03:48:59 -0800

    Project information bar completed

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -409,26 +409,59 @@ class Feed extends React.Component {
                 <div className='project-info-text'>
                   <h3>Project Description</h3>
                   <p>RingIn is a clone of LinkedIn themed around J. R. R. Tolkien's Lord of the Rings series.</p>
-
-                  <ul>
-                    <li>
-                      React
-                    </li>
-                    <li>
-                      Redux
-                    </li>
-                    <li>
-                      Ruby on Rails
-                    </li>
-                    <li>
-                      PostgreSQL
-                    </li>
-                    <li>
-                      AWS S3
-                    </li>
-                  </ul>
-                  
+                  <p>This project is built with...</p>
                 </div>
+
+                <ul>
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>React</span>
+                    </div>
+                    <span className='post-info-item-desc'>Frontend structure</span>
+                  </li>
+
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>Redux</span>
+                    </div>
+                    <span className='post-info-item-desc'>Frontend store</span>
+                  </li>
+
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>Ruby on Rails</span>
+                    </div>
+                    <span className='post-info-item-desc'>Backend</span>
+                  </li>
+
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>PostgreSQL</span>
+                    </div>
+                    <span className='post-info-item-desc'>Database</span>
+                  </li>
+
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>AWS</span>
+                    </div>
+                    <span className='post-info-item-desc'>Media uploading and storage</span>
+                  </li>
+
+                  <li className='post-info-item-wrap'>
+                    <div className='post-info-item-header'>
+                      <span className='post-info-bullet'></span>
+                      <span className='post-info-item'>HTML, CSS, jQuery</span>
+                    </div>
+                    <span className='post-info-item-desc'></span>
+                  </li>
+                </ul>
+
               </section>
 
             </div>
