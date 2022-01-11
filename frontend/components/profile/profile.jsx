2f7d9578dff33898a46e1994d50b46de687e26ff
Vincent Hsu <vincesuuu1@gmail.com>
2022-01-11 03:26:21 -0800

    Refactoring the profile component

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -18,20 +18,6 @@ class Profile extends React.Component {
     this.handleConnectSubmit = this.handleConnectSubmit.bind(this);
   }
 
-  connectionStatusCheck() {
-    if (!this.props.user || !this.props.currentUser) return null;
-
-    let currentUserBool = false;
-
-    if (this.props.currentUser.id === this.props.user.id) {
-      currentUserBool = true;
-    }
-
-    this.setState({ 
-      currentUserStatus: currentUserBool,
-    });
-  }
-
   currentUserCheck() {
     this.props.currentUser.id === this.props.userId ? this.setState({ currentUserStatus: true }) : null;
   }
@@ -41,8 +27,7 @@ class Profile extends React.Component {
       .then(this.currentUserCheck())
       .then(this.props.fetchExperiences(this.props.userId))
       .then(this.props.fetchEducations(this.props.userId))
-      .then(this.props.fetchConnection(this.props.userId, this.props.currentUser.id))
-      .then(this.connectionStatusCheck());
+      .then(this.props.fetchConnection(this.props.userId, this.props.currentUser.id));
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -64,99 +49,112 @@ class Profile extends React.Component {
     this.props.createConnection({...this.state, connectee_id: this.props.userId});
   }
 
-  tempConnectButton() {
-    return (
-      <button onClick={() => this.props.createConnection({...this.state, connectee_id: this.props.userId})}>
-        Connect
-      </button>
-    )
-  }
-
-  tempDeleteButton(connectionId) {
-    let currentlyConnector = this.props.connection.connector_id === this.props.currentUser.id;
-    if (currentlyConnector) {
-      return (
-        <button onClick={() => this.props.deleteConnection(connectionId)}>
-          Pending
-        </button>
-      )
-    } else {
-      return (
-        <div>
-          <button onClick={() => this.props.updateConnection({...this.props.connection, pending: false})}>
-            Accept 
-          </button>
-
-          <button onClick={() => this.props.deleteConnection(connectionId)}>
-            Ignore
-          </button>
-        </div>
-      )
-    }
-  }
-
-  tempDeleteButton2(connectionId) {
-    return (
-      <button onClick={() => this.props.deleteConnection(connectionId)}>
-        Destroy Connection
-      </button>
-    )
-  }
-
   render() {
     if (!this.props.user || !this.props.connection) return null;
 
     return (
       <div className="profile-background">
-        <div className="profile-container">
         <NavbarContainer page="profile" />
+        <div className="profile-container">
+          <div className='profile-content-wrap'>
 
-          <div className="profile-main-wrapper">
-            <div className="profile-main">
-              <div className="profile-card">
-                <img src="https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq" alt="sidebar banner" />
-                <div className="profile-avatar">
-                  {this.props.currentUser.profilePictureUrl ? <img src={this.props.currentUser.profilePictureUrl} alt="user profile picture" /> : <img src="https://static-exp1.licdn.com/sc/h/1c5u578iilxfi4m4dvc4q810q" alt="default profile picture" />}
-                </div>
-                <div className="profile-card-main">
-                  <div className="profile-info">
-                    <h1 className="profile-name">{this.props.user.first_name} {this.props.user.last_name}</h1>
-                    <br />
-                    <h2 className="profile-headline">{this.props.user.headline}</h2>
-                    <h3 className="profile-location">{this.props.user.city_district}, {this.props.user.country_region}</h3>
-                    <div>
-                      {/* Connections Logic */}
-                      {!this.state.currentUserStatus && this.props.connection.error === null ? this.tempConnectButton() : null}
-                      {!this.state.currentUserStatus && this.props.connection.error !== null && this.props.connection.pending ? this.tempDeleteButton(this.props.connection.id) : null}
-                      {!this.state.currentUserStatus && this.props.connection.error !== null && !this.props.connection.pending ? this.tempDeleteButton2(this.props.connection.id) : null}
-
-                    </div>
-                  </div>
-                </div>
+            <section className='profile-content-main'>
+              {/* Banner */}
+              <div className='profile-banner-wrap'>
+                {this.props.user.bannerUrl ? <img className='profile-banner' src={this.props.user.bannerUrl} /> : <img className='profile-banner' src="https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq" />}
               </div>
-              <div className="stat-wrap">
-                <div className="stat-wrap-header">
-                  <h1 className="experience-header">Experience</h1>
-                  <br />
-                  <div className="experience-item">
-                    {this.props.experiences.map(experience => <ExperienceItemContainer key={experience.id} experience={experience} user={this.props.user} />)}
-                    {this.state.currentUserStatus ? this.props.openCreateExperienceModal : null}
+        
+              {/* Profile Info */}
+              <div className='profile-info-wrap'>
+                <div className='profile-info-propic-box'>
+                  <div className='profile-picture-box'>
+                    {this.props.user.profilePictureUrl ? <img /> : <img />}
                   </div>
                 </div>
 
-                <div className="stat-wrap-tail">
-                  <h1 className="education-header">Education</h1>
-                  <br />
-                  <div>
-                    {this.props.educations.map(education => <EducationItemContainer key={education.id} education={education} user={this.props.user} />)}
-                    {this.state.currentUserStatus ? this.props.openCreateEducationModal : null}
-                  </div>
-                </div>
               </div>
+
+            </section>
+
+            {/* Project Bar */}
+            <section className='project-info-bar'>
+              <div className='project-info-text'>
+                <h3>Project Description</h3>
+                <p>RingIn is a clone of LinkedIn themed around J. R. R. Tolkien's The Lord of the Rings.</p>
+                <p>This full stack project is built with...</p>
+              </div>
+
+              <ul>
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>React</span>
+                  </div>
+                  <span className='post-info-item-desc'>Frontend structure</span>
+                </li>
+
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>Redux</span>
+                  </div>
+                  <span className='post-info-item-desc'>Frontend store</span>
+                </li>
+
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>Ruby on Rails</span>
+                  </div>
+                  <span className='post-info-item-desc'>Backend</span>
+                </li>
+
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>PostgreSQL</span>
+                  </div>
+                  <span className='post-info-item-desc'>Database</span>
+                </li>
+
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>AWS</span>
+                  </div>
+                  <span className='post-info-item-desc'>Media uploading and storage</span>
+                </li>
+
+                <li className='post-info-item-wrap'>
+                  <div className='post-info-item-header'>
+                    <span className='post-info-bullet'></span>
+                    <span className='post-info-item'>HTML, CSS</span>
+                  </div>
+                  <span className='post-info-item-desc'></span>
+                </li>
+              </ul>
+            </section>
           </div>
 
-          </div>
+          {/* <div className="stat-wrap">
+            <div className="stat-wrap-header">
+              <h1 className="experience-header">Experience</h1>
+              <br />
+              <div className="experience-item">
+                {this.props.experiences.map(experience => <ExperienceItemContainer key={experience.id} experience={experience} user={this.props.user} />)}
+                {this.state.currentUserStatus ? this.props.openCreateExperienceModal : null}
+              </div>
+            </div>
 
+            <div className="stat-wrap-tail">
+              <h1 className="education-header">Education</h1>
+              <br />
+              <div>
+                {this.props.educations.map(education => <EducationItemContainer key={education.id} education={education} user={this.props.user} />)}
+                {this.state.currentUserStatus ? this.props.openCreateEducationModal : null}
+              </div>
+            </div>
+          </div> */}
         </div>
       </div>
     )
