440acd1998c10ef8b0f00c1b55e74a7ec7eb267d
Vincent Hsu <vincesuuu1@gmail.com>
2022-01-11 23:14:42 -0800

    Building out the profile picture modal

--- frontend/components/profile/profile_picture_modal.jsx
+++ frontend/components/profile/profile_picture_modal.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 export default class ProfilePictureModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1
+    }
+  }
+
   render() {
+    let modalHeader;
+    this.state.page === 1 ? modalHeader = "Add photo" : null;
+    this.state.page === 2 ? modalHeader = "Edit photo" : null;
+    this.state.page === 3 ? modalHeader = "Profile photo" : null;
+
     return (
-      <div>
+      <form>
         
-      </div>
+        {/* Header */}
+        <button className="post-modal-x-box" onClick={this.props.closeModal}><img className="post-modal-x" src={window.xURL} /></button>
+        <div className='post-modal-header'>
+          <h2 className='post-modal-header-text'>{modalHeader}</h2>
+        </div>
+
+
+      </form>
     )
   }
 }
