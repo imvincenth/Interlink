36813d81341487726b5d9f23bd2f4e60a83789ef
Vincent Hsu <vincesuuu1@gmail.com>
2022-01-13 00:41:40 -0800

    Fixed the aspect ratio on the profile pictures across components, styled current user information, working on edit profile modal

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavbarContainer from '../feed/navbar/navbar_container';
 import ExperienceItemContainer from './experience/experience_item_container';
 import EducationItemContainer from './education/education_item_container';
@@ -12,7 +13,10 @@ class Profile extends React.Component {
 
       pending: true,
       connector_id: this.props.currentUser.id,
-      connectee_id: ""
+      connectee_id: "",
+
+      connections: [],
+
     }
 
     this.handleConnectSubmit = this.handleConnectSubmit.bind(this);
@@ -22,12 +26,19 @@ class Profile extends React.Component {
     this.props.currentUser.id === this.props.userId ? this.setState({ currentUserStatus: true }) : null;
   }
 
+  filterConnections() {
+    let tempAccepted = [];
+    this.props.connections.forEach(connection => (connection.connector_id === this.props.user.id || connection.connectee_id === this.props.user.id) && !connection.pending ? tempAccepted.push(connection) : null);
+    this.setState({ connections: [...tempAccepted] });
+  }
+
   componentDidMount() {
     this.props.fetchUser(this.props.userId)
       .then(this.currentUserCheck())
       .then(this.props.fetchExperiences(this.props.userId))
       .then(this.props.fetchEducations(this.props.userId))
-      .then(this.props.fetchConnection(this.props.userId, this.props.currentUser.id));
+      .then(this.props.fetchConnections(this.props.userId))
+      .then(() => this.filterConnections());
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -37,8 +48,12 @@ class Profile extends React.Component {
         .then(this.currentUserCheck())
         .then(this.props.fetchExperiences(this.props.userId))
         .then(this.props.fetchEducations(this.props.userId))
-        .then(this.props.fetchConnection(this.props.userId, this.props.currentUser.id))
-        .then(this.setState({ currentUserStatus: false }));
+        .then(this.props.fetchConnections(this.props.userId))
+        .then(this.setState({ currentUserStatus: false }))
+        .then(() => this.filterConnections());
+    }
+    if (JSON.stringify(prevProps.connections) !== JSON.stringify(this.props.connections)) {
+      this.filterConnections();
     }
   }
 
@@ -48,8 +63,17 @@ class Profile extends React.Component {
     this.props.createConnection({...this.state, connectee_id: this.props.userId});
   }
 
+  renderProfileOptions() {
+    return (
+      <div style={{display: "flex", paddingTop: "8px"}}>
+        <button className='profile-user-option' style={this.props.user !== this.props.currentUser ? {display: "none"} : null}>Add section</button>
+        <button className='profile-user-option'>More</button>
+      </div>
+    )
+  }
+
   render() {
-    if (!this.props.user || !this.props.connection) return null;
+    if (!this.props.user) return null;
 
     return (
       <div className="profile-background">
@@ -60,7 +84,7 @@ class Profile extends React.Component {
             <section className='profile-content-main'>
               {/* Banner */}
               <div className='profile-banner-wrap'>
-                <button className='banner-button-button' onClick={() => this.props.openModal("banner", this.props.user)}><img className='banner-button-img' src={window.bannerButtonURL} /></button>
+                <button className='banner-button-button' onClick={() => this.props.openModal("banner", this.props.user)} style={this.props.currentUser !== this.props.user ? {"display": "none"} : null}><img className='banner-button-img' src={window.bannerButtonURL} /></button>
                 {this.props.user.bannerUrl ? <img onClick={() => this.props.openModal("bannerShow", this.props.user)} className='profile-banner' src={this.props.user.bannerUrl} /> : <img onClick={() => this.props.openModal("bannerShow", this.props.user)} className='profile-banner' src="https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq" />}
               </div>
         
@@ -76,8 +100,35 @@ class Profile extends React.Component {
                   </div>
                 </div>
 
-                <div>
-                  ok cmon
+                <div className='profile-social-wrap'>
+                  <button className='profile-social-info-edit-button' style={this.props.currentUser !== this.props.user ? {"display": "none"} : null} onClick={() => this.props.openModal("editProfile", this.props.user)}><img src={window.quillURL} /></button>
+
+                  {/* User Information */}
+                  <div style={{display: "flex", flexDirection: "column", paddingBottom: "24px"}}>
+                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                      <div style={{display: "flex", flexDirection: "column"}}>
+                        <h1 className='profile-user-name'>{this.props.user.first_name} {this.props.user.last_name}</h1>
+                        <span className='profile-user-headline'>{this.props.user.headline}</span>
+                      </div>
+
+                      <span className='profile-user-school' style={{display: "flex", alignItems: "center"}}><img style={{width: "32px", height: "32px", marginRight: "8px"}} src="https://static-exp1.licdn.com/sc/h/aajlclc14rr2scznz5qm2rj9u" />{this.props.educations[0] ? this.props.educations[0].school : null}</span>
+                    </div>
+
+                    <span className='profile-user-location'>{this.props.user.city_district}, {this.props.user.country_region}</span>
+
+                    {/* User Connections Section */}
+                    <Link to={`/connections`}>
+                      <span className='profile-user-connections'>{this.state.connections.length} Connection{this.state.connections.length > 1 ? "s" : ""}</span>
+                    </Link>
+
+                    {/* User Options */}
+                    <div style={{display: "flex", paddingTop: "8px"}}>
+                      <button className='profile-user-option'>Add section</button>
+                      <button className='profile-user-option'>More</button>
+                    </div>
+
+                  </div>
+                  
                 </div>
               </div>
 
@@ -143,26 +194,28 @@ class Profile extends React.Component {
             </section>
           </div>
 
-          {/* <div className="stat-wrap">
-            <div className="stat-wrap-header">
-              <h1 className="experience-header">Experience</h1>
-              <br />
-              <div className="experience-item">
-                {this.props.experiences.map(experience => <ExperienceItemContainer key={experience.id} experience={experience} user={this.props.user} />)}
-                {this.state.currentUserStatus ? this.props.openCreateExperienceModal : null}
-              </div>
-            </div>
-
-            <div className="stat-wrap-tail">
-              <h1 className="education-header">Education</h1>
-              <br />
-              <div>
-                {this.props.educations.map(education => <EducationItemContainer key={education.id} education={education} user={this.props.user} />)}
-                {this.state.currentUserStatus ? this.props.openCreateEducationModal : null}
-              </div>
-            </div>
-          </div> */}
         </div>
+
+        {/* <div className="stat-wrap">
+          <div className="stat-wrap-header">
+            <h1 className="experience-header">Experience</h1>
+            <br />
+            <div className="experience-item">
+              {this.props.experiences.map(experience => <ExperienceItemContainer key={experience.id} experience={experience} user={this.props.user} />)}
+              {this.state.currentUserStatus ? this.props.openCreateExperienceModal : null}
+            </div>
+          </div>
+
+          <div className="stat-wrap-tail">
+            <h1 className="education-header">Education</h1>
+            <br />
+            <div>
+              {this.props.educations.map(education => <EducationItemContainer key={education.id} education={education} user={this.props.user} />)}
+              {this.state.currentUserStatus ? this.props.openCreateEducationModal : null}
+            </div>
+          </div>
+        </div> */}
+
       </div>
     )
   }
--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -4,7 +4,7 @@ import { fetchExperiences } from '../../actions/experience_actions'
 import { fetchEducations } from '../../actions/education_actions';
 import { fetchUsers, fetchUser } from '../../actions/user_actions';
 import { openModal } from '../../actions/modal_actions';
-import { createConnection, fetchConnection, updateConnection, deleteConnection } from '../../actions/connection_actions';
+import { createConnection, fetchConnections, updateConnection, deleteConnection } from '../../actions/connection_actions';
 import Profile from './profile';
 
 const mSTP = (state, ownProps) => ({
@@ -15,14 +15,14 @@ const mSTP = (state, ownProps) => ({
   educations: Object.values(state.entities.educations),
   experiences: Object.values(state.entities.experiences),
   errors: state.errors.users,
-  connection: Object.values(state.entities.connections)[0]
-  // connection: state.entities.connections
+  // connection: Object.values(state.entities.connections)[0],
+  connections: Object.values(state.entities.connections)
 });
 
 const mDTP = dispatch => ({
   fetchExperiences: userId => dispatch(fetchExperiences(userId)),
   fetchEducations: userId => dispatch(fetchEducations(userId)),
-  fetchConnection: (connecteeId, connectorId) => dispatch(fetchConnection(connecteeId, connectorId)),
+  fetchConnections: userId => dispatch(fetchConnections(userId)),
   fetchUsers: () => dispatch(fetchUsers()),
   fetchUser: user => dispatch(fetchUser(user)),
   openModal: (modalType, user) => dispatch(openModal(modalType, user)),
