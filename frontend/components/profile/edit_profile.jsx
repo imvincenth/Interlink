21efb9946047bac551800d5ade0ed13a0cf8c00f
Vincent Hsu <vincesuuu1@gmail.com>
2022-01-13 03:33:44 -0800

    Finished styling the profile edit modal, trying to debug the handle submit

--- frontend/components/profile/edit_profile.jsx
+++ frontend/components/profile/edit_profile.jsx
@@ -26,49 +26,90 @@ class EditProfileForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <form>
 
-          <label>
-            First name*
-            <input 
-              type="text" 
-              value={this.state.first_name} 
-              onChange={this.update("first_name")} 
-            />
-          </label>
+        {/* Header */}
+        <button className="post-modal-x-box" onClick={this.props.closeModal}><img className="post-modal-x" src={window.xURL} /></button>
+        <div className='post-modal-header' style={{display: "flex", alignItems: "center"}}>
+          <h2 className='post-modal-header-text'>Edit intro</h2>
+        </div>
 
-          <label>
-            Last name*
-            <input 
-              type="text" 
-              value={this.state.last_name} 
-              onChange={this.update("last_name")} 
-            />
-          </label>
+        {/* Edit Profile Content */}
+        <div className='profile-edit-modal-wrap'>
+          <p className='profile-edit-indicates-required'>* Indicates required</p>
+          <div className='profile-edit-modal-name-section'>
 
-          <label>
-            Headline*
-            <input 
-              type="text" 
-              value={this.state.headline}
-              onChange={this.update("headline")}
-            />
-          </label>
+            <div style={{display: "flex", flexDirection: "column", paddingBottom: "32px"}}>
+              <label className='profile-edit-modal-label' htmlFor="profile-edit-first-name">First name<sup>*</sup></label>
+              <input 
+                className='profile-edit-modal-input' 
+                type="text" 
+                id='profile-edit-first-name' 
+                value={this.state.first_name} 
+                onChange={this.update("first_name")} 
+              />
+            </div>
 
-          <h1>Location</h1>
-          <label>
-            Country/Region*
-            <input type="text" value={this.state.country_region} onChange={this.update("country_region")} />
-          </label>
-          <label>
-            City/District*
-            <input type="text" value={this.state.city_district} onChange={this.update("city_district")} />
-          </label>
+            <div style={{display: "flex", flexDirection: "column", paddingBottom: "32px"}}>
+              <label className='profile-edit-modal-label' htmlFor="profile-edit-last-name">Last name<sup>*</sup></label>
+              <input 
+                className='profile-edit-modal-input' 
+                type="text" 
+                id='profile-edit-last-name' 
+                value={this.state.last_name} 
+                onChange={this.update("last_name")} 
+              />
+            </div>
 
-          <input type="submit" onSubmit={this.handleSubmit} value="Save" />
-        </form>
-      </div>
+            <div style={{display: "flex", flexDirection: "column", paddingBottom: "32px"}}>
+              <label className='profile-edit-modal-label' htmlFor="profile-edit-headline">Headline<sup>*</sup></label>
+              <input 
+                className='profile-edit-modal-input' 
+                type="text" 
+                id='profile-edit-headline' 
+                value={this.state.headline} 
+                onChange={this.update("headline")} 
+              />
+            </div>
+
+            <h4>Location</h4>
+
+            <div style={{display: "flex", flexDirection: "column", paddingBottom: "32px"}}>
+              <label className='profile-edit-modal-label' htmlFor="profile-edit-country-region">Country/Region<sup>*</sup></label>
+              <input 
+                className='profile-edit-modal-input' 
+                type="text" 
+                id='profile-edit-country-region' 
+                value={this.state.country_region} 
+                onChange={this.update("country_region")} 
+              />
+            </div>
+
+            <div style={{display: "flex", flexDirection: "column", paddingBottom: "32px"}}>
+              <label className='profile-edit-modal-label' htmlFor="profile-edit-city-district">City/District<sup>*</sup></label>
+              <input 
+                className='profile-edit-modal-input' 
+                type="text" 
+                id='profile-edit-city-district' 
+                value={this.state.city_district} 
+                onChange={this.update("city_district")} 
+              />
+            </div>
+
+          </div>
+        </div>
+
+        <footer 
+          style={{display: "flex", 
+          flexDirection: "row-reverse", 
+          justifyContent: "space-between", 
+          padding: "16px 24px", 
+          borderTop: "1px solid rgba(0, 0, 0, 0.08)"}}
+        >
+          <button className='profile-edit-modal-save' onClick={this.handleSubmit}>Save</button>
+        </footer>
+
+      </form>
     )
   }
 }
